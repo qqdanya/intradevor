2ab9a1e44aec6bb6a3a3cbe749ab71ecba173a47
qqdanya <qqdanya@tuta.io>
2025-09-01 18:36:05 +0700

    nice

--- core/intrade_api_async.py
+++ core/intrade_api_async.py
@@ -77,16 +77,14 @@ async def get_current_percent(
     """
     t = "Classic" if str(trade_type).lower() == "classic" else "Sprint"
     payload = {
-        # "type": t,
-        "type": "Sprint",
+        "type": t,
         "currency_name": account_ccy,
         "investment": str(investment),
-        "percent": "",
+        "percent": "79",
         "option": option.replace("/", ""),
     }
-    # if str(trade_type).lower() == "sprint":
-    # payload["time"] = str(int(minutes)) админы пидарасы
-    payload["time"] = "1"
+    if str(trade_type).lower() == "sprint":
+        payload["time"] = str(int(minutes))
 
     print(payload)
     text = await client.post(PATH_PERCENT, data=payload, expect_json=False)
